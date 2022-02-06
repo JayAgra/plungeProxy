e19306d48a58723c3cceb7b917491a0c5c09eb88
JayAgra <69493224+JayAgra@users.noreply.github.com>
2022-02-06 15:33:41 -0800

    toggle user created accounts

--- admin.php
+++ admin.php
@@ -5,6 +5,12 @@
     if ($isadmin != "0"){
         header('Location: index.php', TRUE, 404);
     } else {
+    include('creationconfig.php');
+    if ($_USRCREATE = 1) {
+        $cboxpos = 'on';
+        } else if ($_USRCREATE = 0) {
+        $cboxpos = 'off';
+        }
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,6 +44,29 @@
             <form action="deletebkg.php" method="post">
             <input class="button-danger" type="submit" value="Delete Image" name="submit">
                 </form>
+			</div>
+			<div  class="main">
+			    <h3>Allow users to create accounts</h3><br>
+			    <form action="" method="post">
+			    <label class="switch">
+                    <input type="checkbox" name="usrcreate" value="<?php echo $cboxpos ?>">
+                    <span class="slider"></span>
+                </label>
+                <div class="formactionrow">                <button class="button-submit" type="submit">Submit</button></div>
+                </form>
+                <?php 
+                if (isset($_POST['usrcreate'])) {
+                    $cfgfle = fopen("creationconfig.php", "w") or die("Unable to open file!");
+                    $addvar = '<?php $_USRCREATE = 1;?>';
+                    fwrite($cfgfle, $addvar);
+                    fclose($cfgfle);
+                } else {
+                    $cfgfle = fopen("creationconfig.php", "w") or die("Unable to open file!");
+                    $addvar = '<?php $_USRCREATE = 0;?>';
+                    fwrite($cfgfle, $addvar);
+                    fclose($cfgfle);
+                }
+                ?>
 			</div>
 </body>
 
